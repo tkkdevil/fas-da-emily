154e3336f7ef821eddf3edfe9cc1a3a3bba3ddb0
tkkdevil <emilylimaa0613@gmail.com>
2022-10-25 20:28:51 -0300

    menu

---
+++ emylimda.py
@@ -0,0 +1,20 @@
+def nomes ():
+    menu = []
+
+    while(True):
+        print("1 cadastro do aluno")
+        print("2 listar todos os alunos")
+        print("3 listar alunos em especifico")
+        print("4 atualizar aluno")
+        print("5 excluir aluno")
+        print("6 media")
+        print("0 sair")
+        
+        numero_da_opcao = int(input("digite o numero da opcao"))
+
+        if(numero_da_opcao =1,7):
+            return menu 
+
+        menu.append(numero_da_opcao)
+
+nomes()
