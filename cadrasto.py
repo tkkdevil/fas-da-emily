a1914b51d607dcc681d2a12062bc2d5e84be61b9
tkkdevil <emilylimaa0613@gmail.com>
2022-11-04 20:29:59 -0300

    auhyye.py

--- cadrasto.py
+++ cadrasto.py
@@ -1,4 +1,31 @@
-arquivo = open("arquivo.txt", "a")
-
 def cadrasto ():
+    arquivo = open("cadrasto.csv", "r") 
     
+    conteudo = arquivo.readlines()
+
+    ultima_linha = conteudo[-1]
+    resultado = ultima_linha.split(";")
+    
+    print(resultado[0] )
+
+    id= int (resultado[0])
+    id = (id + 1)   
+    print(id)
+
+    arquivo.close()
+
+
+    a = input("digite um nome")
+    b = input("digite a matricula")
+    c = input("digite a idade")
+    d = input("digite a nota")
+
+    arquivo = open("cadrasto.csv", "a")
+
+    arquivo.write (str (id) + ";")
+    arquivo.write(a + ";")
+    arquivo.write(b + ";")
+    arquivo.write(c + ";")
+    arquivo.write(d + "\n")
+    arquivo.close()
+cadrasto()
