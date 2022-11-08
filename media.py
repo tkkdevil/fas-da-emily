5083dcb3c67ac1d96b73a5a592a8d189ae84527b
tkkdevil <emilylimaa0613@gmail.com>
2022-11-08 20:14:38 -0300

    media

---
+++ media.py
@@ -0,0 +1,14 @@
+def media ():
+    arquivo = open("cadrasto.csv", "r") 
+    
+    conteudo = arquivo.readlines()
+    a= len(conteudo)
+    soma = 0
+
+    for n in range (2 ,a):
+        calcular = conteudo[n].split(";")
+        receber = int(calcular[4])
+        soma = receber + soma
+    resultado = soma/(a-2)
+    print(resultado)
+media() 
